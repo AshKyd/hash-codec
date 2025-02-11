8cd36efac324a9e766d1adaf57ce773d4761be0d
Ash Kyd <kyd.ashley@abc.net.au>
2025-02-11 11:36:42 +1000

    bug: export correct files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export { default as HashSync } from "./HashSync.js";
 export { default as decodeSchema } from "./decodeSchema.js";
 export { default as encodeSchema } from "./encodeSchema.js";
-export { stringCodec } from "./StringCodec/StringCodec.js";
+export { GzipCodec } from "./GzipCodec/GzipCodec.js";
 export { getRleCodec } from "./RleCodec/RleCodec.js";
